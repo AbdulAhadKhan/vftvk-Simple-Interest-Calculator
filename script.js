1a7ec19cd4bff8c710999f3a9c30310efc5a3b4b
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2021-02-18 13:32:04 +0000

    Highlight result values

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ function compute()
 
     // Display Result
     el = document.getElementById("result");
-    el.innerHTML = "If you deposit " + principal + "<br/>";
-    el.innerHTML += "at an interest rate of " + rate + "%." + "<br/>";
-    el.innerHTML += "You will recieve an amount of " + interest + ",<br/>";
-    el.innerHTML += "in the year " + endYear + ".<br/>";
+    el.innerHTML = "If you deposit <span class='highlight'>" + principal + "</span><br/>";
+    el.innerHTML += "at an interest rate of <span class='highlight'>" + rate + "%." + "</span><br/>";
+    el.innerHTML += "You will recieve an amount of <span class='highlight'>" + interest + "</span>,<br/>";
+    el.innerHTML += "in the year <span class='highlight'>" + endYear + "</span>.<br/>";
 }
 
 function rateUpdate() {
