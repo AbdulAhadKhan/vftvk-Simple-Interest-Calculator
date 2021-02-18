fdd85d6d17bbad48b1743e85f23e048108d6f355
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2021-02-18 12:44:24 +0000

    Added rate update logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // Get  input values
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    // Computations
+    interest = principal * years * rate / 100;
+    endYear = new Date().getFullYear() + years;
 }
-        
+
+function rateUpdate() {
+    rate = document.getElementById("rate").value;
+    document.getElementById("rate-update").innerHTML = rate + "%";
+}
